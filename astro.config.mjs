b227df83393861f628f932c6c2d5c63541252c07
Paige Braun <paigembraun@gmail.com>
2025-07-11 16:52:12 -0500

    update astro config for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     site: "https://paigebraun.github.io",
+    base: "/average-blog/",
     integrations: [mdx(), sitemap(), tailwind()],
 });
